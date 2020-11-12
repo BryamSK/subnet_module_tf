e182e13ae149c7de7921d0c6bd8a14129cc5925c
BryamSK <bryam.hf@gmail.com>
2020-11-12 08:42:47 -0500

    se agrega configuracion para creacion de subredes con ip publica.

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 locals {
   addrs_by_idx  = cidrsubnets(var.base_cidr_block, var.networks[*].new_bits...)
   addrs_by_name = { for i, n in var.networks : n.name => local.addrs_by_idx[i] if n.name != null }
+  map_public_ip_on_launch = treu
   network_objs = [for i, n in var.networks : {
     name       = n.name
     new_bits   = n.new_bits
